ef5b29693cf6a2b7ebd3cd6c24c3ce5d093e5390
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-11 18:39:44 +0530

    Added BaseActivity.kt and modified MainActivity.kt

--- app/src/main/java/com/example/medicinereminderapp/MainActivity.kt
+++ app/src/main/java/com/example/medicinereminderapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.medicinereminderapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.medicinereminderapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
     }
 }
