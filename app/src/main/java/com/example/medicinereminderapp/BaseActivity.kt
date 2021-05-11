0d79a600dca9f34b15f791bfe5129c6b4a509676
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-11 18:44:09 +0530

    Added BaseActivity.kt file and added onClickListener to login button.

--- app/src/main/java/com/example/medicinereminderapp/BaseActivity.kt
+++ app/src/main/java/com/example/medicinereminderapp/BaseActivity.kt
@@ -1,4 +1,4 @@
 package com.example.medicinereminderapp
 
-class BaseActivity  {
+class BaseActivity {
 }
--- app/src/main/java/com/example/medicinereminderapp/MainActivity.kt
+++ app/src/main/java/com/example/medicinereminderapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.medicinereminderapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.example.medicinereminderapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this, BaseActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
