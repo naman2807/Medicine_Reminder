0deeac84fec62c0fabb04c69f8696120f17a4a1d
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-11 18:47:05 +0530

    Added RegisterActivity.kt file and register.xml file

---
+++ app/src/main/java/com/example/medicinereminderapp/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.medicinereminderapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register)
+    }
+}
