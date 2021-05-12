e567c1da467b28f32d6428eb8bcac5961ac5b912
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-12 11:06:19 +0530

    Added first fragment and its layout file.

---
+++ app/src/main/java/com/example/medicinereminderapp/Fragment1.kt
@@ -0,0 +1,11 @@
+package com.example.medicinereminderapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Fragment1 : Fragment() {
+
+}
