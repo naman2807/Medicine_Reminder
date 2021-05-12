664af949a43a79365ab98d2b89703eb6aa164bc2
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-12 11:14:59 +0530

    Created Fragment3.kt file and added onCreateView() method.

---
+++ app/src/main/java/com/example/medicinereminderapp/Fragment3.kt
@@ -0,0 +1,15 @@
+package com.example.medicinereminderapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Fragment3 : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val layout = inflater.inflate(R.layout.fragment_3, container, false)
+        return layout
+    }
+}
