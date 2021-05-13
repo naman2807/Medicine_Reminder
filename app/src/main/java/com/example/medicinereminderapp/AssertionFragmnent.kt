67ada590c687a50ba9b676f0db3ed1339e035e76
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-13 12:54:23 +0530

    Inflated the fragment_assertion.xml in onCreateView() method of AssertionFragment.kt

--- app/src/main/java/com/example/medicinereminderapp/AssertionFragmnent.kt
+++ app/src/main/java/com/example/medicinereminderapp/AssertionFragmnent.kt
@@ -5,7 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.medicinereminderapp.databinding.FragmentAssertionBinding
 
 class AssertionFragmnent : Fragment() {
+        private lateinit var binding : FragmentAssertionBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAssertionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
