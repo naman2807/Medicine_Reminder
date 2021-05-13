4e8d1f3b5ae215ac4599796a24b6c3fcd34ec0c4
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-13 13:08:49 +0530

    Renamed the fragment_3.xml to fragment_reports.xml

--- app/src/main/java/com/example/medicinereminderapp/ReportsFragment.kt
+++ app/src/main/java/com/example/medicinereminderapp/ReportsFragment.kt
@@ -9,7 +9,7 @@ import android.view.ViewGroup
 class ReportsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val layout = inflater.inflate(R.layout.fragment_3, container, false)
+        val layout = inflater.inflate(R.layout.fragment_reports, container, false)
         return layout
     }
 }
