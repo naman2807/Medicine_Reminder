b8713e6fdbe68395bde57722371fdd2264a4d2e7
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-13 12:52:47 +0530

    Inflated the fragment_home.xml in onCreateView() method of HomeFragment.kt

--- app/src/main/java/com/example/medicinereminderapp/HomeFragment.kt
+++ app/src/main/java/com/example/medicinereminderapp/HomeFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.medicinereminderapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
+    private lateinit var binding : FragmentHomeBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
