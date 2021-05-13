905d72468077f78fddfca5d5cff491ea0d8159c7
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-13 13:51:47 +0530

    Extended the AffirmationFragmentAdapter class to RecyclerView.Adapter<AffirmationFragmentAdapter.AffirmationViewHolder>()

--- app/src/main/java/com/example/medicinereminderapp/adapter/AffirmationFragmentAdapter.kt
+++ app/src/main/java/com/example/medicinereminderapp/adapter/AffirmationFragmentAdapter.kt
@@ -2,16 +2,30 @@ package com.example.medicinereminderapp.adapter
 
 import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.medicinereminderapp.R
 import com.example.medicinereminderapp.model.Affirmation
 
-class AffirmationFragmentAdapter(private val context: Context, private val dataList : List<Affirmation>) {
+class AffirmationFragmentAdapter(private val context: Context, private val dataList : List<Affirmation>)
+    : RecyclerView.Adapter<AffirmationFragmentAdapter.AffirmationViewHolder>(){
 
     class AffirmationViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textview : TextView = view.findViewById(R.id.item_title)
         val image : ImageView = view.findViewById(R.id.item_image)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AffirmationViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: AffirmationViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 }
