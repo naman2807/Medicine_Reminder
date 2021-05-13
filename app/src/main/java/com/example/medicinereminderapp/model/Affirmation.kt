e72d62e53b470cb28e77906bb1d478589a700297
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-13 13:42:27 +0530

    Created a new data class Affirmation.kt to store Affirmation data

---
+++ app/src/main/java/com/example/medicinereminderapp/model/Affirmation.kt
@@ -0,0 +1,7 @@
+package com.example.medicinereminderapp.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation (@StringRes val description : Int, @DrawableRes val image : Int){
+}
