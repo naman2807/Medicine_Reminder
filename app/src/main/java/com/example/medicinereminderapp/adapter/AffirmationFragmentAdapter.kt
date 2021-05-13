0b6b5dd191905ae79d2b933a379d7e2e9ba43cbf
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-13 13:50:23 +0530

    Created 2 variables: textview amd image in inner class AffirmationViewHolder(itemView: View)

--- app/src/main/java/com/example/medicinereminderapp/adapter/AffirmationFragmentAdapter.kt
+++ app/src/main/java/com/example/medicinereminderapp/adapter/AffirmationFragmentAdapter.kt
@@ -1,11 +1,17 @@
 package com.example.medicinereminderapp.adapter
 
+import android.content.Context
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.medicinereminderapp.R
+import com.example.medicinereminderapp.model.Affirmation
 
-class AffirmationFragmentAdapter {
+class AffirmationFragmentAdapter(private val context: Context, private val dataList : List<Affirmation>) {
 
-    class AffirmationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+    class AffirmationViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textview : TextView = view.findViewById(R.id.item_title)
+        val image : ImageView = view.findViewById(R.id.item_image)
     }
 }
