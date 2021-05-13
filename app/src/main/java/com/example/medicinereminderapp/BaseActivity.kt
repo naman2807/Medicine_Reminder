444814186c7d5cce7ca5a025b8d3ff834bf1a93e
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-13 13:03:20 +0530

    Renamed the Fragment1.kt to ReminderFragment.kt

--- app/src/main/java/com/example/medicinereminderapp/BaseActivity.kt
+++ app/src/main/java/com/example/medicinereminderapp/BaseActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationMenu
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
@@ -45,7 +44,7 @@ class BaseActivity : AppCompatActivity(){
             lateinit var selectedFragment : Fragment
              when(it.itemId){
                  R.id.bottom_home -> selectedFragment = HomeFragment()
-                 R.id.reminder -> selectedFragment = Fragment1()
+                 R.id.reminder -> selectedFragment = ReminderFragment()
                  R.id.appointment -> selectedFragment = Fragment2()
                  R.id.reports -> selectedFragment = Fragment3()
                  R.id.assertion -> selectedFragment = AssertionFragmnent()
--- app/src/main/java/com/example/medicinereminderapp/ReminderFragment.kt
+++ app/src/main/java/com/example/medicinereminderapp/ReminderFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class Fragment1 : Fragment() {
+class ReminderFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val layout = inflater.inflate(R.layout.fragment_1, container, false)
