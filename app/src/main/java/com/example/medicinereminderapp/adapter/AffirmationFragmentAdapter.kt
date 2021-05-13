a4ab51a09cc9daefd830b0c6f070dfaae83b4517
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-13 13:47:25 +0530

    Created an inner class AffirmationViewHolder(itemView: View) which extends RecyclerView.ViewHolder(itemView)

--- app/src/main/java/com/example/medicinereminderapp/adapter/AffirmationFragmentAdapter.kt
+++ app/src/main/java/com/example/medicinereminderapp/adapter/AffirmationFragmentAdapter.kt
@@ -1,4 +1,11 @@
 package com.example.medicinereminderapp.adapter
 
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
 class AffirmationFragmentAdapter {
+
+    class AffirmationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
 }
