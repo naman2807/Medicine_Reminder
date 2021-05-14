04d1a1bcf480a60c92a61902f1c36ea8bc5f1edd
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-14 13:21:23 +0530

    Created a local variable _selectedFragment and its backing property.

--- app/src/main/java/com/example/medicinereminderapp/viewmodel/BaseActivityViewModel.kt
+++ app/src/main/java/com/example/medicinereminderapp/viewmodel/BaseActivityViewModel.kt
@@ -1,6 +1,14 @@
 package com.example.medicinereminderapp.viewmodel
 
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class BaseActivityViewModel : ViewModel(){
+
+    private var _selectedFragment = MutableLiveData<Fragment>()
+    val selectedFragment : LiveData<Fragment> = _selectedFragment
+
+
 }
