d2f5670706795cc44aca5b24dee326ed35f0266d
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-19 12:22:55 +0530

    Extended the HomeFragmentAdapter.kt with Adapter class.

--- app/src/main/java/com/example/medicinereminderapp/adapter/HomeFragmentAdapter.kt
+++ app/src/main/java/com/example/medicinereminderapp/adapter/HomeFragmentAdapter.kt
@@ -3,7 +3,7 @@ package com.example.medicinereminderapp.adapter
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class HomeFragmentAdapter {
+class HomeFragmentAdapter : RecyclerView.Adapter<HomeFragmentAdapter.HomeViewHolder>(){
 
     class HomeViewHolder(view : View) : RecyclerView.ViewHolder(view){
 
