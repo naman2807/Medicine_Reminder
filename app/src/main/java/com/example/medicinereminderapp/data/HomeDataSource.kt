50e7dccb8396528ff5c921987711fdffd151072b
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-19 12:14:38 +0530

    Added new string called cardiologist

--- app/src/main/java/com/example/medicinereminderapp/data/HomeDataSource.kt
+++ app/src/main/java/com/example/medicinereminderapp/data/HomeDataSource.kt
@@ -1,4 +1,13 @@
 package com.example.medicinereminderapp.data
 
+import com.example.medicinereminderapp.R
+import com.example.medicinereminderapp.model.Categories
+
 class HomeDataSource {
+
+    fun loadItems() : List<Categories>{
+        return listOf<Categories>(
+//                Categories(R.drawable.ic_cardiology, "Cardiologist", )
+        )
+    }
 }
