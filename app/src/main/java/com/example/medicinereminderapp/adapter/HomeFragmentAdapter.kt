83f2c526015f59151dafed076815ebb651edc769
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-19 12:21:54 +0530

    Created a new inner class HomeViewHolder in HomeFragmentAdapter.kt

--- app/src/main/java/com/example/medicinereminderapp/adapter/HomeFragmentAdapter.kt
+++ app/src/main/java/com/example/medicinereminderapp/adapter/HomeFragmentAdapter.kt
@@ -1,4 +1,11 @@
 package com.example.medicinereminderapp.adapter
 
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
 class HomeFragmentAdapter {
+
+    class HomeViewHolder(view : View) : RecyclerView.ViewHolder(view){
+
+    }
 }
