9c5b59b6d1c2f092d6aa9655e2babc195f07f35f
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-19 12:19:27 +0530

    Returned a list of Categories from loadItems() in HomeDataSource.kt

--- app/src/main/java/com/example/medicinereminderapp/data/HomeDataSource.kt
+++ app/src/main/java/com/example/medicinereminderapp/data/HomeDataSource.kt
@@ -7,7 +7,10 @@ class HomeDataSource {
 
     fun loadItems() : List<Categories>{
         return listOf<Categories>(
-                Categories(R.drawable.ic_cardiology, R.string.cardiologist, R.string.specialist)
+                Categories(R.drawable.ic_cardiology, R.string.cardiologist, R.string.specialist),
+                Categories(R.drawable.ic_baseline_remove_red_eye_24, R.string.eye, R.string.specialist),
+                Categories(R.drawable.ic_polio, R.string.polio, R.string.specialist),
+                Categories(R.drawable.ic_vaccine, R.string.vaccine, R.string.specialist)
         )
     }
 }
