89977dc545247a426c7a1c2b89c23118dd6c1538
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-19 12:23:30 +0530

    Overridden all 3 abstract methods of Adapter class

--- app/src/main/java/com/example/medicinereminderapp/adapter/HomeFragmentAdapter.kt
+++ app/src/main/java/com/example/medicinereminderapp/adapter/HomeFragmentAdapter.kt
@@ -1,11 +1,24 @@
 package com.example.medicinereminderapp.adapter
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class HomeFragmentAdapter : RecyclerView.Adapter<HomeFragmentAdapter.HomeViewHolder>(){
 
     class HomeViewHolder(view : View) : RecyclerView.ViewHolder(view){
 
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 }
