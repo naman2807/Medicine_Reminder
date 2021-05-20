f53f38a271ccae245548d4eda0ad5e24a7843d12
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-20 14:14:03 +0530

    Created a new DoctorViewHolder inner class and extend it to RecyclerView.ViewHolder

--- app/src/main/java/com/example/medicinereminderapp/adapter/DoctorAdapter.kt
+++ app/src/main/java/com/example/medicinereminderapp/adapter/DoctorAdapter.kt
@@ -1,7 +1,13 @@
 package com.example.medicinereminderapp.adapter
 
 import android.content.Context
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 import com.example.medicinereminderapp.model.Doctor
 
 class DoctorAdapter(val context : Context, val dataList : List<Doctor>) {
+
+    class DoctorViewHolder(val view : View) : RecyclerView.ViewHolder(view){
+
+    }
 }
