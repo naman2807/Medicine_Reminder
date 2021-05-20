945df3ae54f822c84b40754f13c102fcb7463ebd
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-20 14:15:40 +0530

    Implement all methods of  RecyclerView.Adapter<DoctorAdapter.DoctorViewHolder>()

--- app/src/main/java/com/example/medicinereminderapp/adapter/DoctorAdapter.kt
+++ app/src/main/java/com/example/medicinereminderapp/adapter/DoctorAdapter.kt
@@ -2,6 +2,7 @@ package com.example.medicinereminderapp.adapter
 
 import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.medicinereminderapp.model.Doctor
 
@@ -10,5 +11,17 @@ class DoctorAdapter(val context : Context, val dataList : List<Doctor>)
 
     class DoctorViewHolder(val view : View) : RecyclerView.ViewHolder(view){
 
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DoctorViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: DoctorViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 }
