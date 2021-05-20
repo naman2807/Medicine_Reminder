48e1e05c053d65aba373fc489ca14fd1d692f310
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-20 14:12:42 +0530

    Created a new DoctorAdapter.kt class

---
+++ app/src/main/java/com/example/medicinereminderapp/adapter/DoctorAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.medicinereminderapp.adapter
+
+import android.content.Context
+import com.example.medicinereminderapp.model.Doctor
+
+class DoctorAdapter(val context : Context, val dataList : List<Doctor>) {
+}
