0a22db19202ca25f9ed6d38c784e0d8e65a78d43
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-20 13:47:17 +0530

    Created a new layout file doctor_list_item.xml

--- app/src/main/java/com/example/medicinereminderapp/data/HomeDataSource.kt
+++ app/src/main/java/com/example/medicinereminderapp/data/HomeDataSource.kt
@@ -10,7 +10,7 @@ class HomeDataSource {
                 Categories(R.drawable.ic_cardiology, R.string.cardiologist, R.string.specialist),
                 Categories(R.drawable.ic_baseline_remove_red_eye_24, R.string.eye, R.string.specialist),
                 Categories(R.drawable.ic_polio, R.string.polio, R.string.specialist),
-                Categories(R.drawable.ic_vaccine, R.string.vaccine, R.string.specialist)
+                Categories(R.drawable.ic_vaccine, R.string.vaccine, R.string.specialist),
         )
     }
 }
