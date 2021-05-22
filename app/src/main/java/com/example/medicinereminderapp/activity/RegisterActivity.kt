2032ee96865ad9d0c104d98edcd4ed3078037a72
naman2807 <namanagarwaldbs438@gmail.com>
2021-05-22 14:37:20 +0530

    Applied gradient to register.xml

--- app/src/main/java/com/example/medicinereminderapp/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/medicinereminderapp/activity/RegisterActivity.kt
@@ -1,7 +1,9 @@
 package com.example.medicinereminderapp.activity
 
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.medicinereminderapp.R
 
 class RegisterActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
+
+        val animation : AnimationDrawable = findViewById<ConstraintLayout>(R.id.constraint_reg).background as AnimationDrawable
+        animation.setEnterFadeDuration(1500)
+        animation.setExitFadeDuration(3000)
+        animation.start()
     }
 }
