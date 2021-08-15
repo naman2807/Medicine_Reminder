d6030de692d8904c5cfd715ae8106ce647b2f0a1
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-15 10:53:33 +0530

    Made MedicineReminderDatabase.kt a singleton

--- app/src/main/java/com/example/medicinereminderapp/database/roomdatabase/MedicineReminderDatabase.kt
+++ app/src/main/java/com/example/medicinereminderapp/database/roomdatabase/MedicineReminderDatabase.kt
@@ -1,6 +1,8 @@
 package com.example.medicinereminderapp.database.roomdatabase
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.medicinereminderapp.database.dao.UserDao
 import com.example.medicinereminderapp.model.User
@@ -8,4 +10,23 @@ import com.example.medicinereminderapp.model.User
 @Database(entities = [User::class], version = 1, exportSchema = false)
 abstract class MedicineReminderDatabase: RoomDatabase() {
     abstract fun getUserDao(): UserDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: MedicineReminderDatabase? = null
+
+        fun getDatabase(context: Context): MedicineReminderDatabase{
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MedicineReminderDatabase::class.java,
+                    "medicine_reminder_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
