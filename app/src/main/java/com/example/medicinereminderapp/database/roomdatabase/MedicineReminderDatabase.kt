c0d5d3137c7709464060ee350c65cdb94b728789
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-15 10:50:40 +0530

    Added @Database annotation to  MedicineReminderDatabase.kt class

--- app/src/main/java/com/example/medicinereminderapp/database/roomdatabase/MedicineReminderDatabase.kt
+++ app/src/main/java/com/example/medicinereminderapp/database/roomdatabase/MedicineReminderDatabase.kt
@@ -1,4 +1,8 @@
 package com.example.medicinereminderapp.database.roomdatabase
 
+import androidx.room.Database
+import com.example.medicinereminderapp.model.User
+
+@Database(entities = [User::class], version = 1, exportSchema = false)
 abstract class MedicineReminderDatabase {
 }
