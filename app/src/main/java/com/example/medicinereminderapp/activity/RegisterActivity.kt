0d7f263bdb1061303e24d0effff09158522a7604
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-15 11:41:04 +0530

    Displayed the Toast

--- app/src/main/java/com/example/medicinereminderapp/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/medicinereminderapp/activity/RegisterActivity.kt
@@ -2,11 +2,14 @@ package com.example.medicinereminderapp.activity
 
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.medicinereminderapp.MedicineReminderApplication
 import com.example.medicinereminderapp.R
+import com.example.medicinereminderapp.databinding.RegisterBinding
 import com.example.medicinereminderapp.viewmodel.UserViewModel
 import com.example.medicinereminderapp.viewmodel.UserViewModelFactory
 
@@ -20,6 +23,24 @@ class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register)
+        val binding = RegisterBinding.inflate(LayoutInflater.from(this))
+        val name = binding.nameEditText.text.toString()
+        val address = binding.addressEditText.text.toString()
+        val phoneNumber = binding.phoneEditText.text.toString()
+        val email = binding.emailEditText.text.toString()
+        val userId = binding.userEditText.text.toString()
+        val password = binding.passwordRegisterEditText.text.toString()
+
+        if (!viewModel.isUserValid(name, address, phoneNumber, email, userId, password)) {
+            if (!viewModel.isUserExists(userId)) {
+                viewModel.addNewUser(name, address, phoneNumber, email, userId, password)
+            } else {
+                Toast.makeText(this, "User Already Exists", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(this, "Enter All Details", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
