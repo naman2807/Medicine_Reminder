2f58ac49f78a26e9b0810c3b356cb72c2df55df6
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-15 10:41:23 +0530

    Created new addNewUser() method and annotated it with @Insert

--- app/src/main/java/com/example/medicinereminderapp/database/UserDao.kt
+++ app/src/main/java/com/example/medicinereminderapp/database/UserDao.kt
@@ -1,7 +1,11 @@
 package com.example.medicinereminderapp.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import com.example.medicinereminderapp.model.User
 
 @Dao
 interface UserDao {
+    @Insert
+    suspend fun addNewUser(user: User)
 }
