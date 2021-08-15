3d72fa9ae5bac0f06e156a613b8c8af6d1625885
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-15 10:55:48 +0530

    Created addNewUser() method

--- app/src/main/java/com/example/medicinereminderapp/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/medicinereminderapp/viewmodel/UserViewModel.kt
@@ -2,6 +2,10 @@ package com.example.medicinereminderapp.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.medicinereminderapp.database.dao.UserDao
+import com.example.medicinereminderapp.model.User
 
 class UserViewModel(private val userDao: UserDao): ViewModel() {
+    fun addNewUser(user: User){
+
+    }
 }
