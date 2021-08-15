4e782c974e85c0f035d7a3ead88ec0815844f2c3
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-15 10:34:10 +0530

    Added properties to User.kt

--- app/src/main/java/com/example/medicinereminderapp/model/User.kt
+++ app/src/main/java/com/example/medicinereminderapp/model/User.kt
@@ -1,5 +1,10 @@
 package com.example.medicinereminderapp.model
 
 data class User(
-    val name: String
+    val name: String,
+    val address: String,
+    val phoneNumber: String,
+    val email: String,
+    val userId: String,
+    val password: String
 )
