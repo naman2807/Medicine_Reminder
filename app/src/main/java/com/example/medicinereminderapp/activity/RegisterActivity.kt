c3e2ca83cff205e9de98365986f0617f662dfb09
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-15 10:31:25 +0530

    Created new User.kt class

--- app/src/main/java/com/example/medicinereminderapp/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/medicinereminderapp/activity/RegisterActivity.kt
@@ -11,10 +11,5 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
-
-        val animation : AnimationDrawable = findViewById<ConstraintLayout>(R.id.constraint_reg).background as AnimationDrawable
-        animation.setEnterFadeDuration(1500)
-        animation.setExitFadeDuration(3000)
-        animation.start()
     }
 }
---
+++ app/src/main/java/com/example/medicinereminderapp/model/User.kt
@@ -0,0 +1,4 @@
+package com.example.medicinereminderapp.model
+
+class User {
+}
