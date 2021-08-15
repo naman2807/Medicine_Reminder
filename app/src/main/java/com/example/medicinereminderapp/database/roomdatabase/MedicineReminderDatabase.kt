27298499d9e10ed62456c3b4934601569f524919
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-15 10:51:22 +0530

    Extended MedicineReminderDatabase.kt class to RoomDatabase

--- app/src/main/java/com/example/medicinereminderapp/database/roomdatabase/MedicineReminderDatabase.kt
+++ app/src/main/java/com/example/medicinereminderapp/database/roomdatabase/MedicineReminderDatabase.kt
@@ -1,8 +1,9 @@
 package com.example.medicinereminderapp.database.roomdatabase
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.medicinereminderapp.model.User
 
 @Database(entities = [User::class], version = 1, exportSchema = false)
-abstract class MedicineReminderDatabase {
+abstract class MedicineReminderDatabase: RoomDatabase() {
 }
