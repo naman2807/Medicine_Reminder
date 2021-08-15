7d4a385d89084ba110f368a637f55059a33a1720
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-15 10:56:39 +0530

    Implemented the addNewUser() method

--- app/src/main/java/com/example/medicinereminderapp/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/medicinereminderapp/viewmodel/UserViewModel.kt
@@ -1,11 +1,16 @@
 package com.example.medicinereminderapp.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.medicinereminderapp.database.dao.UserDao
 import com.example.medicinereminderapp.model.User
+import kotlinx.coroutines.launch
 
 class UserViewModel(private val userDao: UserDao): ViewModel() {
-    fun addNewUser(user: User){
 
+    fun addNewUser(user: User){
+        viewModelScope.launch {
+            userDao.addNewUser(user)
+        }
     }
 }
