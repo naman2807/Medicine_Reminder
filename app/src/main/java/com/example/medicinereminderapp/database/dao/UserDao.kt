6bedf0d2eefddd09156af752518ac498cf1d3c23
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-15 10:45:01 +0530

    Annotated updateNewUser() method with @Update

--- app/src/main/java/com/example/medicinereminderapp/database/dao/UserDao.kt
+++ app/src/main/java/com/example/medicinereminderapp/database/dao/UserDao.kt
@@ -1,14 +1,17 @@
-package com.example.medicinereminderapp.database
+package com.example.medicinereminderapp.database.dao
 
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Update
 import com.example.medicinereminderapp.model.User
 
 @Dao
 interface UserDao {
     @Insert
     suspend fun addNewUser(user: User)
+
+    @Update
     suspend fun updateUser(user: User)
 
     @Delete
