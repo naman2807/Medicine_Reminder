967326a7be098ce94da79e0806c33d32b71e18e9
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-15 10:52:15 +0530

    Created getUserDao() method

--- app/src/main/java/com/example/medicinereminderapp/database/roomdatabase/MedicineReminderDatabase.kt
+++ app/src/main/java/com/example/medicinereminderapp/database/roomdatabase/MedicineReminderDatabase.kt
@@ -2,8 +2,10 @@ package com.example.medicinereminderapp.database.roomdatabase
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.medicinereminderapp.database.dao.UserDao
 import com.example.medicinereminderapp.model.User
 
 @Database(entities = [User::class], version = 1, exportSchema = false)
 abstract class MedicineReminderDatabase: RoomDatabase() {
+    abstract fun getUserDao(): UserDao
 }
