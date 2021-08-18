d6daeff577ad5591e63b2ee0fceb8d4cc28d5d91
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-18 13:00:21 +0530

    Moved to the MainActivity

--- app/src/main/java/com/example/medicinereminderapp/activity/BaseActivity.kt
+++ app/src/main/java/com/example/medicinereminderapp/activity/BaseActivity.kt
@@ -1,6 +1,7 @@
 package com.example.medicinereminderapp.activity
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
@@ -10,10 +11,12 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import com.example.medicinereminderapp.MainActivity
 import com.example.medicinereminderapp.R
 import com.example.medicinereminderapp.fragments.*
 import com.example.medicinereminderapp.viewmodel.BaseActivityViewModel
@@ -58,6 +61,11 @@ class BaseActivity : AppCompatActivity(){
                 R.id.log_out -> {
                     edit?.clear()
                     edit?.apply()
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                    startActivity(intent)
                 }
             }
            return@setNavigationItemSelectedListener true
