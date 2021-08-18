673734c2946d0ec97c703d7d517c2c6849f0e8a4
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-18 12:43:28 +0530

    Created local SharedPreference variable

--- app/src/main/java/com/example/medicinereminderapp/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/medicinereminderapp/fragments/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.example.medicinereminderapp.fragments
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -20,7 +21,7 @@ import com.example.medicinereminderapp.viewmodel.UserViewModelFactory
 
 class LoginFragment: Fragment() {
     private lateinit var binding: LoginBinding
-
+    val sharedPreferences: SharedPreferences? = null
 
     private val viewModel: UserViewModel by activityViewModels {
         UserViewModelFactory(
