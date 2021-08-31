eb8c2a64a6fec920592b818e50aa9ac8d3bd52bf
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-31 21:54:46 +0530

    Created updateMedicine() method

--- app/src/main/java/com/example/medicinereminderapp/database/dao/MedicineDao.kt
+++ app/src/main/java/com/example/medicinereminderapp/database/dao/MedicineDao.kt
@@ -3,10 +3,14 @@ package com.example.medicinereminderapp.database.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Update
 import com.example.medicinereminderapp.model.Medicine
 
 @Dao
 interface MedicineDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE )
     suspend fun addMedicine(medicine: Medicine)
+
+    @Update
+    suspend fun updateMedicine(medicine: Medicine)
 }
