de1657fbf0d627273b2a28ec8fc5310fb16a0c86
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-31 22:07:06 +0530

    Implemented addNewMedicine() method

--- app/src/main/java/com/example/medicinereminderapp/viewmodel/MedicineViewModel.kt
+++ app/src/main/java/com/example/medicinereminderapp/viewmodel/MedicineViewModel.kt
@@ -1,11 +1,16 @@
 package com.example.medicinereminderapp.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.medicinereminderapp.database.dao.MedicineDao
 import com.example.medicinereminderapp.model.Medicine
+import kotlinx.coroutines.launch
 
 class MedicineViewModel(private val medicineDao: MedicineDao): ViewModel() {
-    private fun addNewMedicine(medicine: Medicine){
 
+    private fun addNewMedicine(medicine: Medicine){
+        viewModelScope.launch {
+            medicineDao.addMedicine(medicine)
+        }
     }
 }
