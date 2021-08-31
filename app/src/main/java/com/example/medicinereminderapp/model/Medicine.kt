3bc901984630091830b2d8d4189d620b6c810889
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-31 21:51:19 +0530

    Completed Medicine class

--- app/src/main/java/com/example/medicinereminderapp/model/Medicine.kt
+++ app/src/main/java/com/example/medicinereminderapp/model/Medicine.kt
@@ -1,7 +1,13 @@
 package com.example.medicinereminderapp.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Entity
-data class Medicine(val name: String)    {
-}
+@Entity(tableName = "medicine_table")
+data class Medicine(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "medicine_id")
+    val id: Int,
+    @ColumnInfo(name = "medicine_name")
+    val name: String)
