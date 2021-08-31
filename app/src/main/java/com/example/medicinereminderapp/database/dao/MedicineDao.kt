4a364e52b094e9382a8ee8b1ff1ab3308abf5032
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-31 21:54:01 +0530

    Created addMedicine() method

--- app/src/main/java/com/example/medicinereminderapp/database/dao/MedicineDao.kt
+++ app/src/main/java/com/example/medicinereminderapp/database/dao/MedicineDao.kt
@@ -1,7 +1,12 @@
 package com.example.medicinereminderapp.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.example.medicinereminderapp.model.Medicine
 
 @Dao
 interface MedicineDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE )
+    suspend fun addMedicine(medicine: Medicine)
 }
