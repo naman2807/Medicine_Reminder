ae3611d502b7436d63c1b0279daee645a2d81fd6
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-31 21:55:42 +0530

    Created deleteMedicine() method

--- app/src/main/java/com/example/medicinereminderapp/database/dao/MedicineDao.kt
+++ app/src/main/java/com/example/medicinereminderapp/database/dao/MedicineDao.kt
@@ -1,9 +1,6 @@
 package com.example.medicinereminderapp.database.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Update
+import androidx.room.*
 import com.example.medicinereminderapp.model.Medicine
 
 @Dao
@@ -13,4 +10,7 @@ interface MedicineDao {
 
     @Update
     suspend fun updateMedicine(medicine: Medicine)
+
+    @Delete
+    suspend fun deleteMedicine(medicine: Medicine)
 }
