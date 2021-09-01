1e543e90b58576c182588064d0c0bf30fabc18bc
naman2807 <namanagarwaldbs438@gmail.com>
2021-09-01 16:20:03 +0530

    Created getMedicineDao() method

--- app/src/main/java/com/example/medicinereminderapp/database/roomdatabase/MedicineReminderDatabase.kt
+++ app/src/main/java/com/example/medicinereminderapp/database/roomdatabase/MedicineReminderDatabase.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.medicinereminderapp.database.dao.MedicineDao
 import com.example.medicinereminderapp.database.dao.UserDao
 import com.example.medicinereminderapp.model.User
 
 @Database(entities = [User::class], version = 1, exportSchema = false)
 abstract class MedicineReminderDatabase: RoomDatabase() {
     abstract fun getUserDao(): UserDao
+    abstract fun getMedicineDao(): MedicineDao
 
     companion object{
         @Volatile
