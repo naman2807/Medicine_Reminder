aed2eef1cf7091653ede7fcd16d8be17d5366efa
naman2807 <namanagarwaldbs438@gmail.com>
2021-09-03 00:55:18 +0530

    Created viewModel local property

--- app/src/main/java/com/example/medicinereminderapp/fragments/ReminderFragment.kt
+++ app/src/main/java/com/example/medicinereminderapp/fragments/ReminderFragment.kt
@@ -8,12 +8,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.activityViewModels
+import com.example.medicinereminderapp.MedicineReminderApplication
 import com.example.medicinereminderapp.R
 import com.example.medicinereminderapp.databinding.FragmentReminderBinding
+import com.example.medicinereminderapp.viewmodel.MedicineViewModel
+import com.example.medicinereminderapp.viewmodel.MedicineViewModelFactory
 
 class ReminderFragment : Fragment() {
     private var _binding : FragmentReminderBinding? = null
     private val  binding get()  = _binding!!
+
+    private val viewModel: MedicineViewModel by activityViewModels {
+        MedicineViewModelFactory(
+            (activity?.application as MedicineReminderApplication).database.getMedicineDao())
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentReminderBinding.inflate(inflater, container, false)
