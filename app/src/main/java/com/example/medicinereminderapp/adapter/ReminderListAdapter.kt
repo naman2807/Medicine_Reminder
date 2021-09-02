4181725bffa4a02f0686e0cd765a14a6afef2d54
naman2807 <namanagarwaldbs438@gmail.com>
2021-09-03 00:43:25 +0530

    Created template for ReminderListAdapter.kt

--- app/src/main/java/com/example/medicinereminderapp/adapter/ReminderListAdapter.kt
+++ app/src/main/java/com/example/medicinereminderapp/adapter/ReminderListAdapter.kt
@@ -1,4 +1,27 @@
 package com.example.medicinereminderapp.adapter
 
-class ReminderListAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.medicinereminderapp.databinding.FragmentReminderBinding
+import com.example.medicinereminderapp.databinding.ReminderLayoutBinding
+import com.example.medicinereminderapp.model.Medicine
+
+class ReminderListAdapter(private val onItemClicked: (Medicine) -> Unit):
+        ListAdapter<Medicine, ReminderListAdapter.ReminderViewHolder>(DiffCallBack){
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ReminderListAdapter.ReminderViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ReminderListAdapter.ReminderViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    class ReminderViewHolder(private val binding: ReminderLayoutBinding): RecyclerView.ViewHolder(binding.root){
+
+    }
 }
